8abd8a2779d2346918d5aeb7990c5f548ff216d9
mohammed jazil <mohammedjazil31@gmail.com>
2024-03-25 13:11:02 +0530

    sample thread

--- thread_program.py
+++ thread_program.py
@@ -1,13 +1,19 @@
 import time
+import threading
 
 start = time.perf_counter()
 
 def do_something():
     print("sleeping 1 second")
-    time.sleep(10)
+    time.sleep(1)
     print("done sleeping")
-    
-do_something()
+
+t1 = threading.Thread(target=do_something)
+t2 = threading.Thread(target=do_something)
+t1.start()
+t2.start()
+t1.join()
+t2.join()
 
 finish = time.perf_counter()
 print(f"finished in {round(finish-start,2)} seconds")
